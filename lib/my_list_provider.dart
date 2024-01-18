df0adcda6271a552cdabf2b3b2cd9e5c7c180d11
Naveen Belkale <bnaveenbhat@gmail.com>
2024-01-18 17:31:26 +0530

    Initial commit

---
+++ lib/my_list_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'my_list_provider.g.dart';
+@riverpod
+class MyList extends _$MyList {
+  @override
+  List<String> build() => List.generate(20, (index) => "This is message $index")
+      .reversed.toList();
+  void add(String message) {
+    state = [message, ...state];
+  }
+}
---
+++ lib/my_list_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'my_list_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$myListHash() => r'66687b38aba0d50f52e45b0abadf93158946f9c1';
+
+/// See also [MyList].
+@ProviderFor(MyList)
+final myListProvider =
+    AutoDisposeNotifierProvider<MyList, List<String>>.internal(
+  MyList.new,
+  name: r'myListProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$myListHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$MyList = AutoDisposeNotifier<List<String>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
